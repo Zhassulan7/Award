6962e71681eba5d1de8ad789a93b1de7cce24c09
Sadykov Zhassulan <sadykovzhassulan@gmail.com>
2019-12-22 23:37:00 +0600

    Выложил на гитхаб

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Awards.Models;
+using Awards.Models.ViewModels;
+
+namespace Awards.Controllers
+{
+    public class HomeController : Controller
+    {
+        ApplicationContext _context;
+        public HomeController(ApplicationContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var mainPage = new MainPage();
+            mainPage.Users = _context.Users.ToList();
+            mainPage.Awards = _context.Awards.ToList();
+            var usersAwards = _context.UsersAwards.ToList();
+            foreach (var item in mainPage.Users)
+            {
+                item.Awards.Add(new Award { Id = usersAwards.FirstOrDefault(a => a.UserId == item.Id).AwardId});
+            }
+            for (int i = 0; i < mainPage.Users.Count() - 1; i++)
+            {
+                foreach (var item in mainPage.Awards)
+                {
+                    mainPage.Users[i].Awards.FirstOrDefault(a => a.Id == item.Id).Title = item.Title;
+                    mainPage.Users[i].Awards.FirstOrDefault(a => a.Id == item.Id).Description = item.Description;
+                }               
+            }
+            return View(mainPage);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Awards.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Awards.Controllers
+{
+    public class UserController : Controller
+    {
+        ApplicationContext _context;
+        public UserController(ApplicationContext context)
+        {
+            _context = context;
+        }
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+        public IActionResult AddUser(User user)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
